cca751030b463fdba0645bb588e6b0bae9ba8ac0
agusdwimilniadi <agusdwimill@gmail.com>
2020-09-24 14:29:17 +0700

    membuat tombol acak warna

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ ubahWarna.onclick = function(){
     // document.body.setAttribute('class', 'biru-muda');
     document.body.classList.toggle('biru-muda')
 }
+
+
+const acakWarna = document.createElement('button');
+const teksTombol = document.createTextNode('Acak WARNA');
+
+acakWarna.appendChild(teksTombol);
+acakWarna.setAttribute('type', 'button');
+
+ubahWarna.after(acakWarna) //save button dibawah button yang awal
