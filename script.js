f8d86940097820fc69b0914396b4724faf235465
agusdwimilniadi <agusdwimill@gmail.com>
2020-09-24 14:24:40 +0700

    fix sudah selesai ganti warna dengan tag TOOGLE/ apabila di klik class hilang dan di klik lagi class ada

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const ubahWarna = document.getElementById('ubahWarna');
 
 
 ubahWarna.onclick = function(){
-    document.body.style.backgroundColor = 'lightblue';
+    //document.body.style.backgroundColor = 'lightblue';
+    // document.body.setAttribute('class', 'biru-muda');
+    document.body.classList.toggle('biru-muda')
 }
